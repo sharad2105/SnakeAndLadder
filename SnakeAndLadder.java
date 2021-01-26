75afcdde1a423d737716f6144b4d29a76c7b0621
unknown <patilsharad2000@gmail.com>
2021-01-26 09:14:09 +0530

    Used Random Function and get Number Between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,8 +2,7 @@ import java.util.Random;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
-		Random r =new Random();
-      int x=r.nextInt(6);
-        System.out.println("Random generate a dice  :" +x);
+        int dice=(int)(Math.floor(Math.random() * 10 % 6 + 1));
+        System.out.println("Rolling a dice  :" +dice);
     }
 }
